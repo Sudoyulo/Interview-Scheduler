3c4ab75f8dfcaee50eae7da5e645b2803ece7dde
Kevin Tan Yulo <yulokevin@gmail.com>
2022-01-24 14:30:30 -0800

    delete with confirm works

--- src/components/Application.js
+++ src/components/Application.js
@@ -18,15 +18,12 @@ export default function Application(props) {
   const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
-
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-
     });
   }, []);
 
@@ -35,29 +32,39 @@ export default function Application(props) {
   let dailyInterviewers = getInterviewersForDay(state, state.day);
 
   function bookInterview(id, interview) {
-
-    console.log(id, interview);
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
     };
-
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-
     return axios
       .put(`/api/appointments/${id}`, appointment)
       .then((response) => {
-        console.log("hey",response);
         setState({...state, appointments});
-      })
-      
+      }) 
+  }
+
+  function cancelInterview(id) {
+
+    return axios
+    .delete(`/api/appointments/${id}`)
+    .then((response) => {
+      const appointment = {
+        ...state.appointments[id],
+        interview: null
+      }
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+      setState({...state, appointments});
+    })
   }
 
   const calendar = dailyAppointments.map((appointment) => {
-
     const interview = getInterview(state, appointment.interview);
 
     return (
@@ -68,6 +75,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={dailyInterviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
@@ -97,19 +105,3 @@ export default function Application(props) {
     </main>
   );
 }
-
-
-  // const daysUrl = "http://localhost:8001/api/days"
-  //   useEffect(() => {
-  //     axios.get(daysUrl).then(response => {
-  //       setDays([...response.data])
-  //     });
-  // }, [])  <--- my old code
-
-  // const calendar = dailyAppointments.map(appointment => {
-  //   return (
-  //     <Appointment key={appointment.id} {...appointment} />
-  //   )
-  // })
-
-  // const setDays = days => setState(prev => ({ ...prev, days }));
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Empty from "./Empty";
 import useVisualMode from 'hooks/useVisualMode';
 import Form from "./Form";
 import Status from './Status';
+import Confirm from './Confirm';
 
 export default function Appointment(props) {
 
@@ -13,6 +14,8 @@ export default function Appointment(props) {
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -30,6 +33,24 @@ export default function Appointment(props) {
     
   }
 
+  function cancelInterview(){
+
+    transition(DELETING);
+    props.cancelInterview(props.id)
+    .then(() => {
+      console.log("after", props.interview);
+      transition(EMPTY)
+    })
+
+  }
+
+  function confirm(){
+
+    transition(CONFIRM);
+   
+  }
+
+
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -38,10 +59,13 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={confirm}
         />
       )}
       {mode === CREATE && <Form student="" interviewer={null} interviewers={props.interviewers} onSave={save} onCancel={back}/>}
       {mode === SAVING && <Status message="Please wait"/>}
+      {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && <Confirm message="Are you sure?" onCancel={back} onConfirm={cancelInterview}/>}
     </article>
 
   );
